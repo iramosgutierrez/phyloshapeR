0e4075f867b82750c146b30b33effd6e6d616be6
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2023-12-18 11:07:02 +0100

    distance function for fill method

--- R/utils.R
+++ R/utils.R
@@ -31,3 +31,52 @@ get_dist_vect <- function (centroid, shape, nlines){
   distvect<- as.integer(ceiling(distvect))
   return(distvect)
 }
+
+edgedist_fill <- function(tree, distvect, depth.k){
+  
+  if (ape::Ntip(tree)!=length(distvect)){
+    stop(paste0("The number of lines (",length(distvect),
+                ") must match the number of tips in the tree (",
+                ape::Ntip(tree),")"))
+         }
+  table <- data.frame(tree$edge)
+  names(table)<- c("parent", "child")
+  table$depth <- NA_integer_
+  
+  for(i in 1:length(tree$tip.label)){table$depth[table$child==i]<- distvect[i]}
+  table$depth[table$child==length(tree$tip.label)+1] <- 0
+  l <- 1
+  
+  while(any(is.na(table$depth))){
+    for(i in length(tree$tip.label):max(table$parent)){
+      if(i==length(tree$tip.label)+1){next}#root
+      if(!is.na(table$depth[table$child==i])){next}#ya está encontrado
+      
+      subtable <- table[table$parent==i,]
+      if(any(is.na(subtable$depth))){next}
+      min <- min(subtable$depth)
+      # dif <- ceiling((max(subtable$depth)-min(subtable$depth))/2)
+      # val <- min-dif
+      val <- ceiling(depth.k*min)
+      if(val<0){stop(i)}
+      table$depth[table$child==i] <- val
+      
+    }
+    # print(paste0("loop ", l, "; ", nrow(table[is.na(table$depth),]), " remaining"))
+    l <- l+1#Sys.sleep(2);
+  }
+  table$length <- NA_integer_
+  
+  for(i in 1:max(table$parent)){
+    pos <- which(table$child==i)
+    if(i==length(tree$tip.label)+1){next}
+    
+    val.child <- table$depth[table$child==i]
+    par <- table$parent[table$child==i]
+    if(par == length(tree$tip.label)+1){val.par <- 0}else{
+      val.par <- table$depth[table$child==par]}
+    
+    table$length[pos]<- val.child-val.par
+  }
+  return(table)
+}
