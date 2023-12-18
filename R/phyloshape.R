2a2fafae7891e13f4668af12cd9e288b2872eb9a
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2023-12-18 17:29:50 +0100

    documentation and function completion

--- R/phyloshape.R
+++ R/phyloshape.R
@@ -1,30 +1,76 @@
 
-
-phyloshape <- function(tree, shape, centroid=NULL, nlines =360, method="fill", depth.k=0.95){
+#' Create tree to plot
+#'
+#' Create phylogenetic tree with edge lengths according to a shape
+#'
+#' @param tree Phylogenetic tree to midify its branch lengths. If NULL, a tree of random topology will be created. 
+#' The number of tips will be randomly pruned to match 'nlines'
+#' @param shape a SpatialVector object with only one geometry whose contour will be used to shape the phylogeny
+#' @param point a SpatialVector point from where the phylogeny will be plotted. If NULL, the function will use the centroid.
+#' @param nlines number of lines (tips) the resulting phylogeny will include. default is 360.
+#' @param method Method used to calculate the branch lengths. Method 'extend' will mantain 
+#' the original phylogeny shape and add distances just to the tip branches.
+#' Method 'fill' will calculate distances from the tips back to the root. Distances from 
+#' a tip or node to its direct ancestor are calculated using a depth constant.
+#' @param depth.k Applies for method 'fill'. Percentage of branch that separates internal nodes.
+#' Values near to 0 have clustered internal nodes near to the root, thus resemble to a polytomy, 
+#' while values near 1 have internal nodes clustered toward tips. Default value is 0.95.
+#'
+#' @return A phylogenetic tree whose edge lengths will draw the specified shape.
+#'
+#' @export
+#'
+#' @author Ignacio Ramos-Gutiérrez
+#'
+#' @examplesIf interactive()
+#'  tree2 <- phyloshape(tree=NULL, shape,  method = "fill", nlines = 150, depth.k = 0.99)
+#'  ape::plot.phylo(tree2, type = "f", show.tip.label = F)
+#'  ape::tiplabels(pch=16, cex=.5)
+#'
+phyloshape <- function(tree=NULL, shape, point=NULL, nlines =360, method="fill", depth.k=0.95){
   
+  if(is.null(tree)){
+    tree <- ape::rtree(nlines)
+  }
+  if(!ape::is.ultrametric(tree)){tree <- phytools::force.ultrametric(tree, method = "extend")}
   if (!inherits(tree, "phylo")) {stop("The 'tree' object must be a phylo object.")}
   if (!inherits(shape, "SpatVector")) {stop("The 'shape' object must be a SpatVector object.")}
   if ( dim(shape)[1] != 1){stop("The 'shape' object must contain just one polygon.")}
+  if (!method %in% c("fill", "extend")) {stop("'method' should be 'fill' or 'extend'")}
   
-  
-  if(is.null(centroid)){
-    centroid <- terra::centroids(shape)
+  if(is.null(point)){
+    point <- terra::centroids(shape)
   }
+  terra::crs(point) <- terra::crs(shape)
+  
+  if(!(terra::is.related(point, shape, "intersects"))){
+    stop("Layers 'point' and 'shape' does not intersect")
+  }
+  
+  if(ape::Ntip(tree) < nlines){stop("Number of tree tips smaller than 'nlines'.")}
+  if(ape::Ntip(tree) > nlines){
+    message(paste0(
+      "Number of tree tips and 'nlines' do not match. Pruning tree to ", 
+      nlines ," tips"))
+    tree <- ape::keep.tip(tree, sample(tree$tip.label, nlines, F))
+    
+  }
+  
+  if (depth.k>=1 | depth.k <=0){stop("depth.k must be a value between 0 and 1")}
 
 
   
 
-  distvect <- get_dist_vect(centroid, shape, nlines)
+  distvect <- get_dist_vect(point, shape, nlines)
   
   
-  if(method == "fill"){table <- edgedist_fill(tree, distvect, depth.k)}
+  if(method == "fill"){tree.mod <- edgedist_fill(tree, distvect, depth.k)}
+  if(method == "extend"){tree.mod <- edgedist_ext(tree, distvect)}
   
   
-  
-  tree$edge.length <- table$length
+return(tree.mod)
 
   
 }
 
-# plot.phylo(tree, type = "f", show.tip.label = F)
-# tiplabels(pch=16, cex=.5)
+
--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,73 @@
 
+tip.depth <- function(tree, tips){
+  depth <- vector("numeric")
+  for(i in 1:length(tips)){
+    tip <- tips[i]
+    ascends <- ape::nodepath(tree, 1, Ntip(tree)+1)
+    ascend.length <- tree$edge.length[tree$edge[,2]%in%ascends]
+    sum.length <- sum(ascend.length)
+    depth[i]<-sum.length
+  } 
+  return(depth)
+  
+}
+
+progressbar <- function (curr.iter, tot.iter, ini.iter = 1, units = "mins", 
+          msg = NULL) {
+  #FUNCTION COPIED FROM https://github.com/iramosgutierrez/RIRG package
+  curr.iter <- curr.iter - ini.iter + 1
+  tot.iter <- tot.iter - ini.iter + 1
+  if (units == "secs") {
+    d <- 0
+  }
+  else if (units == "hours") {
+    d <- 2
+  }
+  else {
+    d <- 1
+  }
+  if (curr.iter == 1 & !is.null(msg)) {
+    cat(msg, "\n")
+  }
+  if (curr.iter == 1) {
+    st <<- Sys.time()
+    cat(paste0("0%       25%       50%       75%       100%", 
+               "\n", "|---------|---------|---------|---------|", 
+               "\n"))
+  }
+  v <- seq(from = 0, to = 40, by = 40/tot.iter)
+  v <- diff(ceiling(v))
+  v <- cumsum(v)
+  txt <- strrep("*", times = v[curr.iter])
+  txt <- stringr::str_pad(txt, width = 45, side = "right", 
+                          pad = " ")
+  ct <- Sys.time()
+  et <- as.numeric(difftime(ct, st, units = units))/curr.iter * 
+    (tot.iter - curr.iter)
+  et <- round(et, digits = d)
+  txt.end <- paste0(txt, "ETC: ", et, " ", units)
+  if (curr.iter == ini.iter) {
+    txt.end <- paste0(txt, "ETC: ")
+    maxnchar <<- nchar(txt.end)
+  }
+  if (curr.iter == tot.iter) {
+    txt.end <- paste0("*", txt, "DONE")
+  }
+  if (nchar(txt.end) > maxnchar) {
+    maxnchar <<- nchar(txt.end)
+  }
+  txt.end <- stringr::str_pad(txt.end, width = maxnchar, side = "right", 
+                              pad = " ")
+  cat("\r")
+  cat(txt.end)
+  if (curr.iter == tot.iter) {
+    cat("\n")
+  }
+  if (curr.iter == tot.iter) {
+    rm(list = c("st", "maxnchar"), envir = .GlobalEnv)
+  }
+}
+
 
 createlines <- function(x0=0, y0=0, length=1000000, angle=0){
   angle.rad <- angle*pi/180
@@ -9,22 +78,28 @@ createlines <- function(x0=0, y0=0, length=1000000, angle=0){
   return(line)
 }
 
-get_dist_vect <- function (centroid, shape, nlines){
+get_dist_vect <- function (point, shape, nlines){
   
   contour  <- terra::as.lines(shape)
-  centroid.geom <- terra::geom(centroid)
+  point.geom <- terra::geom(point)
+  
+  xext <- abs(terra::ext(shape)[2]-terra::ext(shape)[1])
+  yext <- abs(terra::ext(shape)[4]-terra::ext(shape)[3])
+  mext<- max(xext, yext)
   
   distvect <- vector(mode="numeric")
   
-  for(i in 0:nlines-1){
-    line <- createlines(centroid.geom[1,"x"],centroid.geom[1,"y"],, i/(360/nlines) )
-    line <- vect(rbind(cbind(object=1, part=1, line, hole=0)), type="lines")
+  for(i in 0:(nlines-1)){
+    progressbar(curr.iter = i+1, tot.iter = nlines, 
+                ini.iter = 1, msg = "Calculating distances from point")
+    line <- createlines(point.geom[1,"x"],point.geom[1,"y"],mext, i*(360/nlines) )
+    line <- terra::vect(rbind(cbind(object=1, part=1, line, hole=0)), type="lines")
     int <- terra::intersect(contour, line)
-    x <- ext(int)[1]
-    y <- ext(int)[3]
-    point <- vect(rbind(cbind(object=1, part=1, matrix(c(x,y), nrow=1, ncol=2), hole=0)), type="points")
-    crs(point)<- crs(centroid)
-    dist <- terra::distance(point, centroid)
+    x <- terra::ext(int)[1]
+    y <- terra::ext(int)[3]
+    pt <- terra::vect(rbind(cbind(object=1, part=1, matrix(c(x,y), nrow=1, ncol=2), hole=0)), type="points")
+    terra::crs(pt)<- terra::crs(point)
+    dist <- terra::distance(pt, point)
     distvect[i+1]<- dist[1,1]
     # print(i)
   }
@@ -78,5 +153,31 @@ edgedist_fill <- function(tree, distvect, depth.k){
     
     table$length[pos]<- val.child-val.par
   }
-  return(table)
+  
+  tree$edge.length <- table$length
+  return(tree)
+  
 }
+
+edgedist_ext <- function(tree, distvect){
+  
+  if (ape::Ntip(tree)!=length(distvect)){
+    stop(paste0("The number of lines (",length(distvect),
+                ") must match the number of tips in the tree (",
+                ape::Ntip(tree),")"))
+  }
+
+  depth <- max(tip.depth(tree, 1:ape::Ntip(tree)))
+  convfactor <- min(distvect)/depth
+  tree$edge.length  <-  tree$edge.length*convfactor
+  
+  for(i in 1:length(distvect)){
+    diffdist <- distvect[i]-min(distvect)
+    tree$edge.length[tree$edge[,2]==i] <- tree$edge.length[tree$edge[,2]==i] + diffdist
+  }
+  return(tree)
+  }
+
+
+
+
