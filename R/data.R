e12fb98ea2e44db89f13a565a60dbefd978c8f7f
Ignacio <ignacio.ramosgutierrez@uam.es>
2024-04-17 10:12:34 +0200

    object documentation

---
+++ R/data.R
@@ -0,0 +1,24 @@
+
+#' world
+#'
+#' Example data to create attendance certificates
+#'
+#' @md
+#' @details
+#' A terra PackedSpatVector object.
+#' ```
+#'
+"world"
+
+
+
+
+#' Example phylogeny
+#'
+#' Example phylogeny for phyloshapeR package
+#'
+#' @format A phylogenetic tree with 120 tips and 119 internal nodes
+#' \describe{
+#' Phylogenetic tree with 120 tips.
+#' }
+"exampletree"
